9ab33dc9bd53f7bafb9d3a5f1a9b4ecb854d6f6a
brunoardel <djbrunohardel@hotmail.com>
2023-08-28 23:55:39 -0300

    config electron updater

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
+const { autoUpdater } = require('electron-updater');
+
+let updateInterval = null;
+
 function createWindow() {
   const startUrl =
     process.env.ELECTRON_START_URL ||
@@ -9,6 +13,7 @@ function createWindow() {
       protocol: 'file:',
       slashes: true,
     });
+
   const win = new BrowserWindow({
     width: 800,
     height: 800,
@@ -16,21 +21,62 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
+
   win.loadURL(startUrl);
+
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
       app.quit();
     }
   });
 }
-app.whenReady().then(createWindow);
+
+app.whenReady().then(() => {
+  createWindow();
+
+  autoUpdater.checkForUpdates();
+
+  updateInterval = setInterval(() => {
+    autoUpdater.checkForUpdates();
+  }, 60000);
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+autoUpdater.on('update-available', (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Update Available',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail:
+      'A new version download started. The app will be restarted to install the update.',
+  };
+  dialog.showMessageBox(dialogOpts);
+
+  updateInterval = null;
+});
+
+autoUpdater.on('update-downloaded', (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail:
+      'A new version has been downloaded. Restart the application to apply the updates.',
+  };
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
 });
